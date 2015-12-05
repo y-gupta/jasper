ba9e16b6f6b49f40744288183b4cd8b9f7506691
TJ Hillard <tjhillard@gmail.com>
2015-12-05 05:13:44 -0600

    refactored the front end for the dashboard. Made Vue data OO

--- public/scripts/demo.js
+++ public/scripts/demo.js
@@ -8,37 +8,37 @@ new Vue ({
         if (data.error) {
           console.warn(data.message);
           this.$set('success', false);
-          this.$set('message', data.message);
+          this.$set('current.message', data.message);
           this.$set('configSuccess', false);
         }
         // success
         else {
           // set data
-          this.$set('success', true);
+          this.$set('logs.success', true);
           this.$set('configSuccess', true);
           // Time
           var time = data[0].time / 1000;
-          this.$set('lastUpdated', moment.unix(time).fromNow());
+          this.$set('current.lastUpdated', moment.unix(time).fromNow());
           // Successes
-          this.$set('successes', data[0].summary.successes);
+          this.$set('current.summary.successes', data[0].summary.successes);
           // Warnings
-          this.$set('warnings', data[0].summary.warnings);
+          this.$set('current.summary.warnings', data[0].summary.warnings);
           // Errors
-          this.$set('errors', data[0].summary.errors);
+          this.$set('current.summary.errors', data[0].summary.errors);
           // FailedPages
-          this.$set('failedPages', data[0].failedPages);
+          this.$set('current.failedPages', data[0].failedPages);
           // Average Reponse Time
-          this.$set('averageResponseTime', data[0].averageResponseTime);
+          this.$set('current.averageResponseTime', data[0].averageResponseTime);
           // isUp. Errors?
           if (data[0].summary.errors > 0) {
-            this.$set('isUp', false);
-            this.$set('message', 'Uh oh! Jasper has found ' + data[0].summary.errors + ' issue(s) with the following pages:');
-            this.$set('statusColor', 'red');
+            this.$set('current.isUp', false);
+            this.$set('current.message', 'Uh oh! Jasper has found ' + data[0].summary.errors + ' issue(s) with the following pages:');
+            this.$set('current.statusColor', 'red');
           }
           else {
-            this.$set('isUp', true);
-            this.$set('message', 'Everything\'s looking good! Jasper is showing ' + data[0].summary.errors + ' issues. Sit back and relax, we will let you know if something comes up.');
-            this.$set('statusColor', 'green');
+            this.$set('current.isUp', true);
+            this.$set('current.message', 'Everything\'s looking good! Jasper is showing ' + data[0].summary.errors + ' issues. Sit back and relax, we will let you know if something comes up.');
+            this.$set('current.statusColor', 'green');
           }
         }
     })
@@ -54,7 +54,7 @@ new Vue ({
         }
         // success
         else {
-          this.$set('baseUrl', data.main.baseUrl);
+          this.$set('config.baseUrl', data.main.baseUrl);
         }
     })
     .error(function(data, status, request) {
@@ -63,34 +63,56 @@ new Vue ({
 
     // GET /outages
     this.$http.get('api/outages/', function(data, status, request) {
-      for (var i = 0; i < 3; i++) {
-        var time = moment.unix(data[i].time / 1000).format('MMMM Do YYYY, h:mm:ss a');
-        var object = {
-          data: data[i]
-        };
-        object.time = time ;
-        this.outages.push(object);
+      if (data.length <= 0) {
+        this.$set('outages.isEmpty', true);
+        console.log('isEmpty true');
       }
+      else {
+        console.log('isEmpty > false');
+        this.$set('outages.isEmpty', false);
+        for (var i = 0; i < data.length; i++) {
+          var time = moment.unix(data[i].time / 1000).format('MMMM Do YYYY, h:mm:ss a');
+          var object = {
+            data: data[i]
+          };
+          object.time = time;
+          this.outages.list.push(object);
+          console.log(this.outages.list);
+        }
+      }
+
     })
     .error(function(data, status, request) {
-
+      console.warn('Uh oh. Something went wrong with the GET /outages request.');
     });
   },
 
   data: {
-    success: false,
+    logs: {
+      succes: false
+    },
     configSuccess: null,
-    message: null,
-    statusColor: null,
-    baseUrl: null,
-    failedPages: null,
-    isUp: null,
-    successes: null,
-    warnings: null,
-    errors: null,
-    lastUpdated: null,
-    averageResponseTime: null,
-    outages: []
+    current: {
+      message: '',
+      statusColor: '',
+      isUp: null,
+      summary: {
+        successes: null,
+        warnings: null,
+        errors: null
+      },
+      failedPages: null,
+      lastUpdated: null,
+      averageResponseTime: null
+    },
+    config: {
+      baseUrl: ''
+    },
+    outages: {
+      list: [],
+      isEmpty: true,
+      emptyMessage: 'No recent outages!'
+    }
   }
 });
 
@@ -101,5 +123,4 @@ $('.circle')
     target   : '.circle',
     title    : 'Response Times',
     content  : 'This speed reflects the time in miliseconds that it took the server to fetch the body content of the specified site. If you are testing manually on your own you may notice discrepencies based off of client/server locations.'
-  })
-;
+  });
